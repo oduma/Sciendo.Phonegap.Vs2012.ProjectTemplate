d385d94df098830fd68379ae413b3404423dffbc
oduma <tavi_duma2002@yahoo.co.uk>
2013-12-05 21:15:45 +0000

    Initial commit

---
+++ Sciendo.PhonegapWizard/FileHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+
+namespace Sciendo.PhonegapWizard
+{
+    public class FileHandler : IWizard
+    {
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary,
+                               WizardRunKind runKind, object[] customParams)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            bool retValue = filePath != "Placeholder.txt";
+            return retValue;
+        }
+
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void RunFinished()
+        {
+        }
+    }
+}
